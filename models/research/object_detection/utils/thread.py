2bb3eabb346952e3584912f8eb0a77970dd58165
Buğra Gülay <bugragulay66@gmail.com>
2019-06-10 01:01:55 +0300

    Version 1.0.0 has been uploaded

---
+++ models/research/object_detection/utils/thread.py
@@ -0,0 +1,23 @@
+# import numpy as np
+# import os
+# import six.moves.urllib as urllib
+# import sys
+# import tarfile
+# import tensorflow as tf
+# import zipfile
+#
+# import abc
+# import collections
+# import functools
+# # Set headless-friendly backend.
+# import matplotlib; matplotlib.use('Agg')  # pylint: disable=multiple-statements
+# import matplotlib.pyplot as plt  # pylint: disable=g-import-not-at-top
+# import numpy as np
+# import PIL.Image as Image
+# import PIL.ImageColor as ImageColor
+# import PIL.ImageDraw as ImageDraw
+# import PIL.ImageFont as ImageFont
+# import six
+# import tensorflow as tf
+#
+# from utils import visualization_utils as vis_util
